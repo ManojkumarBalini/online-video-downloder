f2ac0ee778fb87f1214920a5546a477289cacbc2
ManojkumarBalini <157731041+ManojkumarBalini@users.noreply.github.com>
2025-08-15 21:42:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ echo "Installing yt-dlp..."
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
 chmod a+rx bin/yt-dlp
 
-# Install ffmpeg
+# Install ffmpeg (fixed URL)
 echo "Installing ffmpeg..."
-curl -L -o ffmpeg.tar.xz "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz"
+curl -L -o ffmpeg.tar.xz "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2024-08-26-14-00/ffmpeg-N-115001-g0d00e1c3a1-linux64-gpl.tar.xz"
 tar -xf ffmpeg.tar.xz
-find . -type f -name ffmpeg -exec mv {} bin/ \;
+find . -type d -name "ffmpeg-*" -exec mv {}/ffmpeg bin/ \;
 chmod a+rx bin/ffmpeg
 
 # Cleanup
-rm -rf ffmpeg-master-latest-linux64-gpl ffmpeg.tar.xz
+rm -rf ffmpeg-* ffmpeg.tar.xz
 
 echo "Binaries installed:"
 ls -lh bin/
