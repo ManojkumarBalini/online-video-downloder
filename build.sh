4bce270c65ead872d4277eb4c18af9d08ec69b6e
ManojkumarBalini <157731041+ManojkumarBalini@users.noreply.github.com>
2025-08-15 21:27:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@
 mkdir -p bin
 
 # Install yt-dlp
+echo "Installing yt-dlp..."
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
 chmod a+rx bin/yt-dlp
 
 # Install ffmpeg
+echo "Installing ffmpeg..."
 curl -L -o ffmpeg.tar.xz "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz"
 tar -xf ffmpeg.tar.xz
 find . -type f -name ffmpeg -exec mv {} bin/ \;
